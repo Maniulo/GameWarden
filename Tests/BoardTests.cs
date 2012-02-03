f81a675bbfbe95e41e206773fad9e64157f7a0c4
Mikhail Mezhenin <mail.to.maniac@gmail.com>
2012-02-04 01:03:23 +0600

    Empty solution.

---
+++ Tests/BoardTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameWarden.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
